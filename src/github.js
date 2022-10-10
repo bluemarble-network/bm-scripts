efa26fd58e5fb5fde16576c62a7a1f74a2887d52
bluemarble-personal <bluemarblenetwork@gmail.com>
2022-10-10 12:34:14 -0300

    fix: repositories per page increased to 100

--- src/github.js
+++ src/github.js
@@ -1,28 +1,30 @@
-import axios from "axios"
-import { config } from 'dotenv'
+import axios from "axios";
+import { config } from "dotenv";
 
-config()
+config();
 
 export const github = () => {
-  const TOKEN = process.env.GITHUB_TOKEN
+  const TOKEN = process.env.GITHUB_TOKEN;
   const api = axios.create({
     auth: {
-      username: 'bluemarble-network',
-      password: TOKEN
-    }
-  })
+      username: "bluemarble-network",
+      password: TOKEN,
+    },
+  });
 
   const repos = async (field) => {
-    const { data } = await api.get('https://api.github.com/user/repos')
+    const { data } = await api.get(
+      "https://api.github.com/user/repos?per_page=100"
+    );
 
     if (field) {
-      const reposFormatados = data.map(item => item[field])
+      const reposFormatados = data.map((item) => item[field]);
 
-      return reposFormatados
-    } else return data
-  }
+      return reposFormatados;
+    } else return data;
+  };
 
   return {
-    repos
-  }
-}
+    repos,
+  };
+};
